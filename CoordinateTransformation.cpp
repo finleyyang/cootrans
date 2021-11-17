a71a159f0a5eea981aff0b8c1d18b9b4703498fc
smartmore desktop <finleyyang@163.com>
2021-11-17 16:30:20 +0800

    first

---
+++ CoordinateTransformation.cpp
@@ -0,0 +1,4 @@
+//
+// Created by finley on 17/11/2021.
+//
+
---
+++ cmake-build-debug/CMakeFiles/Coordinate_transformation.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Coordinate_transformation.dir/CoordinateTransformation.cpp.o"
+  "CMakeFiles/Coordinate_transformation.dir/main.cpp.o"
+  "Coordinate_transformation"
+  "Coordinate_transformation.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Coordinate_transformation.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
